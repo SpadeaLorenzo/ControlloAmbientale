b69d0115a857a8d268bacc71105b50334a0f0748
SpadeaLorenzo <62604900+SpadeaLorenzo@users.noreply.github.com>
2022-02-10 10:06:02 +0100

    Update schema.sql

--- DB/schema.sql
+++ DB/schema.sql
@@ -1,46 +1,27 @@
 CREATE DATABASE controllo_ambientale;
 
-use controllo_ambientale;
+USE controllo_ambientale;
 
---finshino's data--
-CREATE TABlE fishino(
-    id INT AUTO_INCREMEN PRIMARY KEY,
-    MAC_address VARCHAR(20) UNIQUE,
-    luogo VARCHAR(30)
+CREATE TABLE user(
+    unsername VARCHAR(20) PRIMARY KEY,
+    pwd CHAR(255),
 );
 
---brightness sensor data--
-CREATE TABLE luce(
-    dato VARCHAR(10),
-    DATETIME cur_time,
-    fishino_id INT,
-    FOREIGN KEY(fishino_id) REFERENCES fishino(id),
-    PRIMARY KEY(cur_time,fishino_id)
+CREATE TABLE fihsino(
+    id INT AUTO_INCREMENT PRIMARY KEY,
+    luogo VARCHAR(40),
+    nome VARCHAR(20) UNIQUE,
+    fishino_username VARCHAR(20),
+    FOREIGN KEY fihsino_username REFERENCES fishino(username)
 );
 
---umidity sensor data--
-CREATE TABLE umidita(
-    dato varchar(10),
-    DATETIME cur_time,
-    fishino_id INT,
-    FOREIGN KEY(fishino_id) REFERENCES fishino(id),
-    PRIMARY KEY(cur_time,fishino_id)
-);
-
---sound sensor data--
-CREATE TABLE souno(
-    dato varchar(10),
-    DATETIME cur_time,
-    fishino_id INT,
-    FOREIGN KEY(fishino_id) REFERENCES fishino(id),
-    PRIMARY KEY(cur_time,fishino_id)
-);
-
---air sensor data--
-CREATE TABLE CO2(
-    dato varchar(10),
-    DATETIME cur_time,
-    fishino_id INT,
-    FOREIGN KEY(fishino_id) REFERENCES fishino(id),
-    PRIMARY KEY(cur_time,fishino_id)
+CREATE TABLE datas(
+    data DATE,
+    fihsino_id INT,
+    humidity INT,
+    brightness INT,
+    sound INT,
+    C02 INT,
+    FOREIGN KEY fihsino_id REFERENCES fishino(id),
+    PRIMARY KEY(data, fihsino_id)
 );
