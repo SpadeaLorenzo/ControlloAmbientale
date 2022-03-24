45424172bab39698240c7e906995e21a1565330a
Erik <59686856+ErikPelloni@users.noreply.github.com>
2022-03-24 16:27:26 +0100

    Aggiornamento sketch

---
+++ ControlloAmbientale/ControlloAmbientale.fish
@@ -0,0 +1,51 @@
+<?xml version="1.0" encoding="UTF-8" standalone="yes" ?>
+<!DOCTYPE FishIDE>
+<FishIDE>
+	<Files>
+		<item>ControlloAmbientale.ino</item>
+		<item>ArduinoJson.h</item>
+	</Files>
+	<Libraries>
+		<key>DHT sensor library</key>
+		<value value="1"/>
+		<key>Wire</key>
+		<value value="1"/>
+		<key>Adafruit Unified Sensor</key>
+		<value value="1"/>
+		<key>Adafruit TSL2561</key>
+		<value value="1"/>
+		<key>FishinoSdFat</key>
+		<value value="1"/>
+		<key>Fishino</key>
+		<value value="1"/>
+	</Libraries>
+	<OtherFiles>
+		<item>C:/Users/Admin/Desktop/ads/ads.ino</item>
+		<item>C:/Users/Admin/Desktop/ControlloAmbientale/ControlloAmbientale.ino</item>
+	</OtherFiles>
+	<OpenedFiles>
+		<key>[PROJECT]\ControlloAmbientale.ino</key>
+		<value>
+			<Cursor value="179"/>
+			<ScrollBar value="0"/>
+		</value>
+		<key>C:/Users/Admin/Desktop/ControlloAmbientale/ControlloAmbientale.ino</key>
+		<value>
+			<Cursor value="4307"/>
+			<ScrollBar value="144"/>
+		</value>
+	</OpenedFiles>
+	<CurrentFile>C:/Users/Admin/Desktop/ControlloAmbientale/ControlloAmbientale.ino</CurrentFile>
+	<SerialMonitor>
+		<Port>COM3</Port>
+		<Speed value="9600"/>
+		<Content>Light Sensor Test
+
+------------------------------------
+Gain:         Auto
+Timing:       13 ms
+------------------------------------
+
+</Content>
+	</SerialMonitor>
+</FishIDE>
